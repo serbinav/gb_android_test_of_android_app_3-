b2a14226087d9c9a564c23b3e7d0f3afdc6d1836
andrey <a.serbin@litebox.ru>
2022-05-17 23:25:41 +0400

    divided MainActivity code into fake and real flavors, add homework description, make a separate strings file for fake and real test

---
+++ app/src/fake/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -0,0 +1,121 @@
+package com.geekbrains.tests.view.search
+
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView.OnEditorActionListener
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.geekbrains.tests.R
+import com.geekbrains.tests.model.SearchResult
+import com.geekbrains.tests.presenter.RepositoryContract
+import com.geekbrains.tests.presenter.search.PresenterSearchContract
+import com.geekbrains.tests.presenter.search.SearchPresenter
+import com.geekbrains.tests.repository.FakeGitHubRepository
+import com.geekbrains.tests.view.details.DetailsActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity(), ViewSearchContract {
+
+//    1. Покройте тестами MainActivity
+//    (кроме элементов RecyclerView — список мы будем тестировать позже):
+//    убедитесь, что все элементы видны и корректно отображаются,
+//    что надписи на кнопке и в EditText верные и т. д.;
+//    2. Избавьтесь от проверки BuildConfig.TYPE == FAKE и
+//    разнесите разный код по разным классам в зависимости от Build Variants,
+//    как вы это делали на 12 занятии курса Андроид на Котлине.
+
+//    *. Передавайте Repository в конструктор Презентера через injection.
+//    Используйте для этого Dagger или Coin.
+//    *. Убедитесь, что тесты отрабатывают без ошибок после ваших изменений.
+//    *. Напишите Espresso-тесты для приложения, которое у вас уже есть в портфолио.
+
+    private val adapter = SearchResultAdapter()
+    private lateinit var presenter: PresenterSearchContract<ViewSearchContract>
+    private var totalCount: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        presenter = SearchPresenter(createRepository())
+        setUI()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.attachView(this)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.detachView(this)
+    }
+
+    private fun setUI() {
+        toDetailsActivityButton.setOnClickListener {
+            startActivity(DetailsActivity.getIntent(this, totalCount))
+        }
+        setQueryListener()
+        setRecyclerView()
+    }
+
+    private fun setRecyclerView() {
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = adapter
+    }
+
+    private fun setQueryListener() {
+        searchEditText.setOnEditorActionListener(OnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                val query = searchEditText.text.toString()
+                if (query.isNotBlank()) {
+                    presenter.searchGitHub(query)
+                    return@OnEditorActionListener true
+                } else {
+                    Toast.makeText(
+                        this@MainActivity,
+                        getString(R.string.enter_search_word),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    return@OnEditorActionListener false
+                }
+            }
+            false
+        })
+    }
+
+    private fun createRepository(): RepositoryContract {
+        return FakeGitHubRepository()
+    }
+
+    override fun displaySearchResults(
+        searchResults: List<SearchResult>,
+        totalCount: Int
+    ) {
+        with(totalCountTextView) {
+            visibility = View.VISIBLE
+            text =
+                String.format(Locale.getDefault(), getString(R.string.results_count), totalCount)
+        }
+
+        this.totalCount = totalCount
+        adapter.updateResults(searchResults)
+    }
+
+    override fun displayError() {
+        Toast.makeText(this, getString(R.string.undefined_error), Toast.LENGTH_SHORT).show()
+    }
+
+    override fun displayError(error: String) {
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun displayLoading(show: Boolean) {
+        if (show) {
+            progressBar.visibility = View.VISIBLE
+        } else {
+            progressBar.visibility = View.GONE
+        }
+    }
+}
