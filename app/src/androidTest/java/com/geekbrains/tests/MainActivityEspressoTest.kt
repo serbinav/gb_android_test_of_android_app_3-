a8c822fa899eed223e609a1de14681a9a068affe
andrey <a.serbin@litebox.ru>
2022-05-17 23:20:40 +0400

    update test and androidTest in project

---
+++ app/src/androidTest/java/com/geekbrains/tests/MainActivityEspressoTest.kt
@@ -0,0 +1,69 @@
+package com.geekbrains.tests
+
+import android.view.View
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.geekbrains.tests.view.search.MainActivity
+import org.hamcrest.Matcher
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityEspressoTest {
+
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun activitySearch_IsWorking() {
+        onView(withId(R.id.searchEditText)).perform(click())
+        onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
+        onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
+
+        onView(isRoot()).perform(delay())
+        onView(withId(R.id.totalCountTextView)).check(matches(withText(TestUtils.getString(R.string.test_result))))
+    }
+
+    private fun delay(): ViewAction? {
+        return object : ViewAction {
+            override fun getConstraints(): Matcher<View> = isRoot()
+            override fun getDescription(): String = "wait for $2 seconds"
+            override fun perform(uiController: UiController, v: View?) {
+                uiController.loopMainThreadForAtLeast(2000)
+            }
+        }
+    }
+
+//    searchEditText.
+//    заглушка без данных
+//
+//    searchEditText.
+//    ввели текст
+//
+//    toDetailsActivityButton
+//    кнопка видна и доступна для нажатия
+//
+//    toDetailsActivityButton
+//    текст кнопки
+//
+//    toDetailsActivityButton
+//    после нажатия на кнопку видны
+//    - и + и текст
+
+    @After
+    fun close() {
+        scenario.close()
+    }
+}
